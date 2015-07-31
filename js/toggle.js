68148f2f971c0b0b5c99cdc26fac6d7f5838c9d9
Nkosana <nkosana24@gmail.com>
2015-07-31 12:29:53 +0200

    Updated toggle and css

--- js/toggle.js
+++ js/toggle.js
@@ -1,11 +1,13 @@
 toggle();
-window.onresize = function() {
-	toggle();
-}
-function toggle() {
-	if (window.innerWidth < 480 && window.innerHeight < 800) {
-		document.getElementById('generated-table').style.display = "none";
-	} else {
-		document.getElementById('generated-table').style.display = "block";
+	window.onresize = function() {
+	    toggle();
 	}
-}
+	function toggle() {
+	    if (window.innerWidth < 480 && window.innerHeight < 800) {
+	        document.getElementById('generated-table').style.display = "none";     
+	        document.getElementByTagName('showTable').style.display = "none";     
+	    }else{
+	    	document.getElementById('generated-table').style.display = "block"; 
+	    	document.getElementByTagName('showTable').style.display = "block"; 
+	    }
+	}
